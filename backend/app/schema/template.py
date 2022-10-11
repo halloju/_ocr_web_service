d9bac43cad6246ae68911df8113f59ee9877c535
Li Li Cheng <d40614d@gmail.com>
2022-10-11 11:01:21 +0800

    [update] 新增 I/O 的 Spec

--- backend/app/schema/template.py
+++ backend/app/schema/template.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class CreateTemplateRequest(BaseModel):
@@ -8,6 +9,23 @@ class CreateTemplateRequest(BaseModel):
     bbox: List = Field(..., title='使用者拉框留存的範本資訊', example=[{'type': 'text', 'tag': '姓名', 'x_min': 0, 'y_min': 0, 'x_max': 100, 'y_max': 100}, {'type': 'box', 'tag': '是否為範本', 'x_min': 130, 'y_min': 200, 'x_max': 200, 'y_max': 270}, {'type': 'mask', 'tag': None, 'x_min': 0, 'y_min': 0, 'x_max': 100, 'y_max': 100}])
     template_name: str = Field(..., title='範本名稱', example='身分證')
 
+class CreateTemplateResponse(BaseModel):
+    template_id: str = Field(..., title="範本影像編號", example="13520220930134411")
+
+class AvailableTemplates(BaseModel):
+    template_id: str = Field(..., title="範本影像編號", example="13520220930134411")
+    template_name: str = Field(..., title='範本名稱', example='身分證')
+    updated_at: datetime = Field(..., title='更新時間', example='2022-10-04T05:12:14.084870+00:00')
+
+class GetAvailableTemplatesResponse(BaseModel):
+    available_templates: List[AvailableTemplates] = Field(..., title="user_id 可取用的 template", example=[])
+
+
+class GetTemplateDetailResponse(BaseModel):
+    image: str = Field(..., title='範本影像', example='')
+    template_name: str = Field(..., title='範本名稱', example='身分證')
+    bbox: List = Field(..., title='使用者拉框留存的範本資訊', example=[{'type': 'text', 'tag': '姓名', 'x_min': 0, 'y_min': 0, 'x_max': 100, 'y_max': 100}, {'type': 'box', 'tag': '是否為範本', 'x_min': 130, 'y_min': 200, 'x_max': 200, 'y_max': 270}, {'type': 'mask', 'tag': None, 'x_min': 0, 'y_min': 0, 'x_max': 100, 'y_max': 100}])
+
 
 class UpdateTemplateRequest(BaseModel):
     template_id: str = Field(..., title="範本影像編號", example="13520220930134411")
@@ -15,7 +33,4 @@ class UpdateTemplateRequest(BaseModel):
     template_name: str = Field(..., title='範本名稱', example='身分證')
 
 class DeleteTemplateRequest(BaseModel):
-    template_id: str = Field(..., title="範本影像編號", example="13520220930134411")
-
-class CreateTemplateResponse(BaseModel):
     template_id: str = Field(..., title="範本影像編號", example="13520220930134411")
