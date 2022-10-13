58291debaaaa43d650e488cfe7246055bfb4d19d
Li Li Cheng <d40614d@gmail.com>
2022-10-13 17:24:38 +0800

    [update] create_template 時新增上傳圖檔至 minio 的功能

--- backend/app/services/template/create.py
+++ backend/app/services/template/create.py
@@ -1,8 +1,11 @@
 from app.models.template_info import TemplateInfo
 from app.exceptions import CustomException
+from app.services import minio
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
+import base64
 from datetime import datetime
+from io import BytesIO
 import logging
 
 
@@ -15,11 +18,23 @@ def create_template(template, db: Session):
     將 template 其餘資訊寫入 DB
     """
     try:
+        today = datetime.today()
+        template_id=template.user_id+today.strftime('%Y%m%d%H%M%S')
+
+        image_base64_binary = template.image.encode('utf-8')
+        image_binary = base64.b64decode(image_base64_binary)
+
         # Step 1. 將 template 影像寫入 MinIO
+        client = minio.get_minio_client()
+        minio.check_bucket(client)
+        client.put_object(
+            minio.BUCKET_NAME,
+            f'template/{today.year:04}/{today.month:02}/{today.day:02}/{today.hour:02}/{today.minute:02}/{template_id}',
+            BytesIO(image_binary),
+            len(image_binary)
+        )
 
         # Step 2. 將 template 其餘資訊寫入 DB
-        today = datetime.today()
-        template_id=template.user_id+today.strftime('%y%m%d%H%M%S')
         template_info = TemplateInfo(
             template_id=template_id,
             user_id=template.user_id,
