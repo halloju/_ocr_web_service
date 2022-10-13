88495ceb28f987bcd82507cca4c1aa0bc17540e0
Li Li Cheng <d40614d@gmail.com>
2022-10-13 17:23:25 +0800

    [update] get_template_detail 時新增從 minio 拉圖並回傳的功能

--- backend/app/services/template/read.py
+++ backend/app/services/template/read.py
@@ -1,7 +1,9 @@
 from app.models.template_info import TemplateInfo
 from app.exceptions import CustomException
+from app.services import minio
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
+import base64
 import logging
 
 
@@ -32,6 +34,18 @@ def get_template_detail(db: Session, template_id: str):
     """
     try:
         # Step 1. 從 MinIO 拉圖出來
+        client = minio.get_minio_client()
+        try:
+            response = client.get_object(
+                minio.BUCKET_NAME,
+                f'template/{template_id[5:9]}/{template_id[9:11]}/{template_id[11:13]}/{template_id[13:15]}/{template_id[15:17]}/{template_id}'
+            )
+            image_binary = response.read()
+        finally:
+            response.close()
+            response.release_conn()
+        base64_binary = base64.b64encode(image_binary)
+        image_base64 = base64_binary.decode('utf-8')
 
         # Step 2. 從 DB 拉座標點等資訊
         template_detail = db.query(
@@ -39,7 +53,7 @@ def get_template_detail(db: Session, template_id: str):
             TemplateInfo.bbox).filter(TemplateInfo.template_id == template_id)
         if not template_detail.first():
             raise CustomException(status_code=400, message="template_detail is not found")
-        return template_detail.first()
+        return image_base64, template_detail.first()
     except OperationalError as e:
         logger.error("db error:{}".format(e))
         raise CustomException(status_code=424, message="[DB Error] 請聯絡管理者")
