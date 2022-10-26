8611e1b2de78a6233c8f972197bc7f3dff04cb1b
erik1110 <oceanuheart@gmail.com>
2022-10-26 18:00:44 +0800

    [feat] 調整 navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,428 +1,165 @@
 <template>
-<div class="page-wrapper">
- <div class="nav-wrapper">
-  <nav class="navbar">
-    <img src="https://static.overlay-tech.com/assets/a29acb82-a927-42fa-ba0a-59c8194dc5bb.svg" alt="logo" class="ocr-logo">
-    <div class="ocr-title">Esun. OCR</div>
-    <div class="menu-toggle" id="mobile-menu">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-    </div>
-    <ul class="nav no-search">
-      <li class="nav-item">
-        <img
-          alt=""
-          class="vector"
-          src="https://static.overlay-tech.com/assets/0c80f190-377f-45a2-a5ce-e120498cbfad.svg"
-        />
-        <p class="">通用文件識別功能</p>
-      </li>
-      <li class="nav-item">
-        <img
-          alt=""
-          class="vector-two"
-          src="https://static.overlay-tech.com/assets/57ef4a3c-d5fc-4a5f-888d-01e66bce4afb.svg"
-        />
-        <p class="">自定義模板</p>
-      </li>
-      <li class="nav-item">
-        <img
-          alt=""
-          class="vector"
-          src="https://static.overlay-tech.com/assets/687cd02f-3a17-4439-8274-a745fff98f94.svg"
-        />
-        <p class="">特殊文件識別功能</p>
-      </li>
-    </ul>
-    <div class="user-info">
-      <div class="group-137">
-        <div class="ghost-two">
-          <div class="ghost">
-            <div class="vector-three"></div>
-            <div class="vector-three"></div>
+  <div class="page-wrapper">
+    <div class="nav-wrapper">
+      <nav class="menu">
+        <!-- <div class="navbar-toggle">
+          <i class="fa fas fa-bars"></i>
+         </div> -->
+        <a href="#" class="logo">
+          <img src="https://static.overlay-tech.com/assets/a29acb82-a927-42fa-ba0a-59c8194dc5bb.svg" alt="logo">
+          <span>Esun.OCR</span>
+        </a>
+        <ul class="navbar">
+          <li class="navbar-links">
+              <img src="https://static.overlay-tech.com/assets/0c80f190-377f-45a2-a5ce-e120498cbfad.svg"/>
+              <p>通用文件識別功能</p>
+          </li>
+          <li class="navbar-links">
+            <img src="https://static.overlay-tech.com/assets/57ef4a3c-d5fc-4a5f-888d-01e66bce4afb.svg"/>
+            <p>自定義模板</p>
+          </li>
+          <li class="navbar-links">
+            <img src="https://static.overlay-tech.com/assets/687cd02f-3a17-4439-8274-a745fff98f94.svg"/>
+            <p>特殊文件識別功能</p>
+          </li>
+        </ul>
+        <div class="user">
+          <div class="group-circle">
+            <div class="user-icon"></div>
+          </div>
+          <div class="relative-wrapper-one">
+            <p class="esb">ESB22005</p>
+            <p class="admin-account">Admin account</p>
           </div>
         </div>
-      </div>
-      <div class="relative-wrapper-one">
-        <p class="emp_id">ESB12345</p>
-        <p class="admin-account">Admin account</p>
-      </div>
+        <div class="navbar-toggle">
+            <i class="fa fas fa-bars"></i>
+        </div>
+      </nav>
     </div>
-  </nav>
-  </div>
-</div>   
+  </div>   
 </template>
 <style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Lato', 'Arial', sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
 }
-
-/* HEADINGS */
-
-h1, p {
-  color: #fff;
-  text-align: center;
-  line-height: 1.4;
-}
-
-h1 {
-  font-size: 2.2rem;
-}
-
-h2 {
-  color: #000;
-  font-size: 1.3rem;
-  text-align: center;
-  line-height: 1.4;
-  margin-bottom: 10px;
-}
-
-/* BASIC SETUP */
-
-.page-wrapper {
-  width: 100%;
-  height: auto;
-}
-
-.nav-wrapper {
-  width: 100%;
-  position: -webkit-sticky; /* Safari */
-  position: sticky;
-  top: 0;
-  background-color: #008b8b;
-}
-
-/* NAVIGATION */
-
-.navbar {
-  font-weight: bold;
+.menu {
   display: flex;
-  justify-content: space-around;
-  grid-template-columns: 1fr 3fr;
-  align-items: flex-start;
-  height: 50px;
-  overflow: hidden;
+  background-color: #008b8b;
+  height: 48px;
+  width: 100%;
+  align-items: center;
+  justify-content: flex;
+  padding: 2px;
 }
-.navbar .ocr-title {
-  max-width: 200px;
-  font-family: "Poppins";
-  font-size: 20px;
-  font-weight: 700;
-  line-height: normal;
-  color: #fff;
+.logo {
+  display: flex;
+  align-items: center;
+  width: 220px;
+  margin-left: 10px;
   margin-right: 10px;
 }
-.navbar img {
-  height: 30px;
-  width: auto;
-  justify-self: start;
-  margin-left: 20px;
-  margin-top: 10px;
+.logo span {
+  margin-left: 15px;
+  color: white;
+  font-weight: bold;
+  font-size: 1.7rem;
 }
-.navbar ul {
-  list-style: none;
-  display: grid;
-  grid-template-columns: repeat(6,1fr);
-  justify-self: end;
+.navbar {
+  display: flex;
+  flex: auto;
+  justify-content: flex-end;
 }
-
-.nav-item a {
-  color: #000;
-  font-size: 0.9rem;
-  font-weight: 400;
-  text-decoration: none;
-  transition: color 0.3s ease-out;
+.navbar li {
+  display: flex;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-left: 25px;
+  margin-right: 25px;
 }
-
-.nav-item a:hover {
-  color: #3498db;
+.navbar li img{
+  max-width: 27px;
 }
-
-.ghost {
-  background-size: 100% 100%;
-  background-position: center;
-  background-image: url("https://static.overlay-tech.com/assets/cbffd0be-e5e1-4257-9b0c-93d9640e17bd.svg");
-  padding: 6.83px 4.67px 8.26px;
+.navbar li p {
+  color: white;
+  margin-left: 10px;
+}
+.navbar-links:hover {
+  background-color: #578F8D;
+}
+.navbar-user:hover {
+  background-color: #578F8D;
+}
+.user {
+  width: 200px;
   display: flex;
   align-items: center;
 }
-.relative-wrapper-one {
-  margin-top: 17.53px;
-  position: relative;
-}
-.emp_id {
-  max-width: 69px;
-  font-family: "Poppins";
-  font-size: 14px;
-  font-weight: 600;
-  line-height: normal;
-  color: $white;
-  position: absolute;
-  left: 0;
-  top: -17px;
-}
-.admin-account {
-  max-width: 108px;
-  color: black;
-  position: relative;
-}
-/* SECTIONS */
-
-.headline {
-  width: 100%;
-  height: 50vh;
-  min-height: 350px;
-  background: linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.7)), url('https://images.unsplash.com/photo-1435224668334-0f82ec57b605?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80');
-  background-size: cover;
+.group-circle {
+  background-color: #FFC145;
+  margin-right: 10px;
+  border-radius: 50%;
+  padding: 3px 3px 3px 3px;
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+}
+.user-icon {
+  background-size: 100% 100%;
+  background-position: center;
+  background-image: url("https://static.overlay-tech.com/assets/cbffd0be-e5e1-4257-9b0c-93d9640e17bd.svg");
+  padding: 8px 11px;
+  display: flex;
   justify-content: center;
 }
-
-.features {
-  width: 100%;
-  height: auto;
-  background-color: #f1f1f1;
-  display: flex;
-  padding: 50px 20px;
-  justify-content: space-around;
+.relative-wrapper-one p {
+  color: white;
+  font-weight: bold;
 }
-
-.feature-container {
-  flex-basis: 30%;
-  margin-top: 10px;
-}
-
-.feature-container p {
-  color: #000;
-  text-align: center;
-  line-height: 1.4;
-  margin-bottom: 15px;
-}
-
-.feature-container img {
-  width: 100%;
-  margin-bottom: 15px;
-}
-
-/* SEARCH FUNCTION */
-
-#search-icon {
-  font-size: 0.9rem;
-  margin-top: 3px;
-  margin-left: 15px;
-  transition: color 0.3s ease-out;
-}
-
-#search-icon:hover {
-  color: #3498db;
-  cursor: pointer;
-}
-
-.search {
-  transform: translate(-35%);
-  -webkit-transform: translate(-35%);
-  transition: transform 0.7s ease-in-out;
-  color: #3498db;
-}
-
-.no-search {
-  transform: translate(0);
-  transition: transform 0.7s ease-in-out;
-}
-
-.search-input {
-  position: absolute;
-  top: -4px;
-  right: -125px;
-  opacity: 0;
-  z-index: -1;
-  transition: opacity 0.6s ease;
-}
-
-.search-active {
-  opacity: 1;
-  z-index: 0;
-}
-
-input {
-  border: 0;
-  border-left: 1px solid #ccc;
-  border-radius: 0; /* FOR SAFARI */
-  outline: 0;
-  padding: 5px;
-}
-
-/* MOBILE MENU & ANIMATION */
-
-.menu-toggle .bar{
-  width: 25px;
-  height: 3px;
-  background-color: #3f3f3f;
-  margin: 5px auto;
-  -webkit-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-}
-
-.menu-toggle {
-  justify-self: end;
-  margin-right: 25px;
+.navbar-toggle {
   display: none;
 }
-
-.menu-toggle:hover{
-  cursor: pointer;
-}
-
-#mobile-menu.is-active .bar:nth-child(2){
-  opacity: 0;
-}
-
-#mobile-menu.is-active .bar:nth-child(1){
-  -webkit-transform: translateY(8px) rotate(45deg);
-  -ms-transform: translateY(8px) rotate(45deg);
-  -o-transform: translateY(8px) rotate(45deg);
-  transform: translateY(8px) rotate(45deg);
-}
-
-#mobile-menu.is-active .bar:nth-child(3){
-  -webkit-transform: translateY(-8px) rotate(-45deg);
-  -ms-transform: translateY(-8px) rotate(-45deg);
-  -o-transform: translateY(-8px) rotate(-45deg);
-  transform: translateY(-8px) rotate(-45deg);
-}
-
-/* KEYFRAME ANIMATIONS */
-
-@-webkit-keyframes gradbar {
-	0% {
-		background-position: 0% 50%
-	}
-	50% {
-		background-position: 100% 50%
-	}
-	100% {
-		background-position: 0% 50%
-	}
-}
-
-@-moz-keyframes gradbar {
-	0% {
-		background-position: 0% 50%
-	}
-	50% {
-		background-position: 100% 50%
-	}
-	100% {
-		background-position: 0% 50%
-	}
-}
-
-@keyframes gradbar {
-	0% {
-		background-position: 0% 50%
-	}
-	50% {
-		background-position: 100% 50%
-	}
-	100% {
-		background-position: 0% 50%
-	}
-}
-
-/* Media Queries */
-
-  /* Mobile Devices - Phones/Tablets */
-
-@media only screen and (max-width: 720px) { 
-  .features {
-    flex-direction: column;
-    padding: 50px;
-  }
-  
-  /* MOBILE HEADINGS */
-  
-  h1 {
-    font-size: 1.9rem;
-  }
-  
-  h2 {
-    font-size: 1rem;
-  }
-  
-  p {
-    font-size: 0.8rem;
-  }
-  
-  /* MOBILE NAVIGATION */
-     
-  .navbar ul {
+@media (max-width: 1130px) {
+  .navbar {
+    position: absolute;
+    top: 48px;
+    right: 0%;
+    width: 270px;
+    height: 30xvh;
+    background-color: #008b8b;
     display: flex;
     flex-direction: column;
-    position: fixed;
-    justify-content: start;
-    top: 55px;
-    background-color: #fff;
-    width: 100%;
-    height: calc(100vh - 55px);
-    transform: translate(-101%);
-    text-align: center;
-    overflow: hidden;
+    justify-content: flex-start;
+    border-radius: 10px;
   }
-  
   .navbar li {
-    padding: 15px;
+    display: flex;
+    margin: 30px 0;
+    padding: 0px 25px;
   }
-  
-  .navbar li:first-child {
-    margin-top: 50px;
+  .navbar-links {
+    width: 100%;
+    text-align: center;
   }
-  
-  .navbar li a {
-    font-size: 1rem;
+  .navbar-links:hover {
+  background-color: #578F8D;
   }
-   
-  .menu-toggle, .bar {
-    display: block;
-    cursor: pointer;
+  .fa {
+    color: white;
   }
-  
-  .mobile-nav {
-  transform: translate(0%)!important;
+  .navbar-toggle {
+    display: flex;
+    font-size: 35px;
+  }
+  .user {
+    width: 200px;
+    display: flex;
+    justify-content: center;
+  }
 }
-  
-  /* SECTIONS */
-  
-  .headline {
-    height: 20vh;
-  }
-    
-  .feature-container p {
-    margin-bottom: 25px;
-  }
-  
-  .feature-container {
-    margin-top: 20px;
-  }
-  
-  .feature-container:nth-child(2) {
-    order: -1;
-  }
-  
-  /* SEARCH DISABLED ON MOBILE */
-  
-  #search-icon {
-    display: none;
-  }
-  
-  .search-input {
-  display: none;
- }
-  
-}
+
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,5 @@
 <template>
-  <div class="d-lg-flex align-items-center justify-content-center my-3">
-    <img src="../assets/images/main.svg" alt="" class="mx-5" />
-    <div class="mx-5">
-      <p class="color-primary mb-2">Fintech Taipei 期間限定</p>
-      <h1 class="color-primary">智能投資健檢</h1>
-      <p class="my-2">市場變動時，AI告訴您如何調整資產配置</p>
-      <router-link to="/question" class="btn bg-primary mt-4" @click="home">立即體驗</router-link>
-    </div>
-  </div>
-  <div class="notice container mt-5 p-4">
-    <h4 class="mb-2">注意事項</h4>
-    <p class="notice_content">
-      以上所提供之分析資訊僅供參考，本行盡力提供正確模擬結果，但對其完整性、即時性和正確性不做任何擔保。任何人因信賴此等資料而做出或改變投資決策，應審慎衡量本身之需求及投資風險，並就投資結果自行負責。本行茲依據「個人資料保護法」及其他相關法令之規定，說明蒐集、處理、利用及國際傳輸申請人個人資料之管理方針，以及申請人所享有之權利或服務，詳依   玉山官網公告之「<span class="color-primary">個人資料運用法定告知事項</span>」及「<span class="color-primary">顧客資料保密措施聲明</span>」為準。
-    </p>
-  </div>
+
 </template>
 
 <style scoped>
