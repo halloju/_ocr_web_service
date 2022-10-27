351dfdf0a4dcc0fc572a604898a3d89198ddbc11
erik1110 <oceanuheart@gmail.com>
2022-10-27 10:27:29 +0800

    [feat] add dropdown for navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,44 +1,61 @@
 <template>
-  <div class="page-wrapper">
-    <div class="nav-wrapper">
-      <nav class="menu">
-        <!-- <div class="navbar-toggle">
-          <i class="fa fas fa-bars"></i>
-         </div> -->
-        <a href="#" class="logo">
-          <img src="https://static.overlay-tech.com/assets/a29acb82-a927-42fa-ba0a-59c8194dc5bb.svg" alt="logo">
-          <span>Esun.OCR</span>
-        </a>
-        <ul class="navbar">
-          <li class="navbar-links">
-              <img src="https://static.overlay-tech.com/assets/0c80f190-377f-45a2-a5ce-e120498cbfad.svg"/>
-              <p>通用文件識別功能</p>
-          </li>
-          <li class="navbar-links">
-            <img src="https://static.overlay-tech.com/assets/57ef4a3c-d5fc-4a5f-888d-01e66bce4afb.svg"/>
-            <p>自定義模板</p>
-          </li>
-          <li class="navbar-links">
-            <img src="https://static.overlay-tech.com/assets/687cd02f-3a17-4439-8274-a745fff98f94.svg"/>
-            <p>特殊文件識別功能</p>
-          </li>
-        </ul>
-        <div class="user">
-          <div class="group-circle">
-            <div class="user-icon"></div>
-          </div>
-          <div class="relative-wrapper-one">
-            <p class="esb">ESB22005</p>
-            <p class="admin-account">Admin account</p>
-          </div>
-        </div>
-        <div class="navbar-toggle">
-            <i class="fa fas fa-bars"></i>
-        </div>
-      </nav>
+<nav class="menu">
+  <a href="#" class="logo">
+    <img src="https://static.overlay-tech.com/assets/a29acb82-a927-42fa-ba0a-59c8194dc5bb.svg" alt="logo">
+    <span>Esun.OCR</span>
+  </a>
+  <div class="navbar">
+    <Dropdown :img_src="img_src1" title="通用文件識別功能" :items="services" class="navbar-links"/>
+    <Dropdown :img_src="img_src2" title="自定義模板" :items="services" class="navbar-links"/>
+    <Dropdown :img_src="img_src3" title="特殊文件識別功能" :items="services" class="navbar-links"/>
+  </div>
+  <div class="user">
+    <div class="group-circle">
+      <div class="user-icon"></div>
     </div>
-  </div>   
+    <div class="relative-wrapper-one">
+      <p class="esb">ESB22005</p>
+      <p class="admin-account">Admin account</p>
+    </div>
+  </div>
+  <div class="navbar-toggle">
+      <i class="fa fas fa-bars"></i>
+  </div>
+</nav>
 </template>
+
+<script>
+import Dropdown from '../components/Dropdown.vue';
+export default {
+name: 'navbar',
+components: {
+  Dropdown
+},
+data () {
+  return {
+    img_src1: 'https://static.overlay-tech.com/assets/0c80f190-377f-45a2-a5ce-e120498cbfad.svg',
+    img_src2: 'https://static.overlay-tech.com/assets/57ef4a3c-d5fc-4a5f-888d-01e66bce4afb.svg',
+    img_src3: 'https://static.overlay-tech.com/assets/687cd02f-3a17-4439-8274-a745fff98f94.svg',
+    services: [
+      {
+        title: 'Web',
+        link: '#',
+        img_src: '',
+      },
+      {
+        title: 'Design',
+        link:'#'
+      },
+      {
+        title: 'Videos',
+        link: '#'
+      }
+    ]
+  }
+}
+}
+</script>
+
 <style>
 * {
   margin: 0;
@@ -52,18 +69,19 @@
   height: 48px;
   width: 100%;
   align-items: center;
-  justify-content: flex;
+  justify-content: flex-start;
   padding: 2px;
 }
 .logo {
   display: flex;
   align-items: center;
-  width: 220px;
-  margin-left: 10px;
-  margin-right: 10px;
+  width: 200px;
+  margin-left: 15px;
+  margin-right: 15px;
 }
 .logo span {
-  margin-left: 15px;
+  margin-left: 10px;
+  margin-bottom: 5px;
   color: white;
   font-weight: bold;
   font-size: 1.7rem;
@@ -73,28 +91,20 @@
   flex: auto;
   justify-content: flex-end;
 }
-.navbar li {
-  display: flex;
-  font-weight: bold;
-  font-size: 1.2rem;
-  margin-left: 25px;
-  margin-right: 25px;
-}
-.navbar li img{
+.navbar img {
   max-width: 27px;
 }
-.navbar li p {
-  color: white;
-  margin-left: 10px;
-}
-.navbar-links:hover {
-  background-color: #578F8D;
-}
-.navbar-user:hover {
-  background-color: #578F8D;
+.navbar-links  {
+  display: flex;
+  justify-content: center;
+  height: 48px;
+  font-weight: bold;
+  font-size: 1.1rem;
+  margin-left: 5px;
+  margin-right: 5px;
 }
 .user {
-  width: 200px;
+  width: 180px;
   display: flex;
   align-items: center;
 }
@@ -123,7 +133,39 @@
 .navbar-toggle {
   display: none;
 }
-@media (max-width: 1130px) {
+
+nav .menu-item {
+  color: #FFF;
+  padding: 10px 20px;
+  position: relative;
+  text-align: center;
+  border-bottom: 3px solid transparent;
+  display: flex;
+  transition: 0.4s;
+  background-color: #008b8b;
+}
+nav .menu-item.active,
+nav .menu-item:hover {
+  background-color: #444;
+  border-bottom-color: #FF5858;
+}
+nav .menu-item a {
+  color: inherit;
+  text-decoration: none;
+}
+@media (max-width: 1200px) {
+  .navbar img {
+    max-width: 20px;
+  }
+  .navbar-links  {
+    justify-content: flex-end;
+    height: 48px;
+    font-size: 1rem;
+    margin-left: 3px;
+    margin-right: 3px;
+  }
+}
+@media (max-width: 1080px) {
   .navbar {
     position: absolute;
     top: 48px;
@@ -161,5 +203,4 @@
     justify-content: center;
   }
 }
-
 </style>
