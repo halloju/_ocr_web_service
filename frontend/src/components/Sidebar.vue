d96005038c1439b436af0456334ea354355f4d9a
erik1110 <oceanuheart@gmail.com>
2022-11-01 18:29:20 +0800

    [style] 調整 sidebar 寬度

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -57,8 +57,8 @@ export default {
 .side-btn {
     border: none;
     height: 80px;
-    width: 250px;
-    padding: 10px 50px;
+    max-width: 300px;
+    padding: 10px 70px;
     cursor: pointer;
     font-size: 21px;
     font-weight: 500;
@@ -82,12 +82,15 @@ export default {
     font-weight: bold;
     margin-left: 0.7vw;
     border-radius: 50px 0 0 50px;
+    z-index: 0;
 }
 .side-btn.active::before {
     top: -30px;
+    
 }
 .side-btn.active::after {
     bottom: -30px;
+    
 }
 .side-btn.active::before, .side-btn.active::after {
     position: absolute;
@@ -96,13 +99,16 @@ export default {
     height: 30px;
     width: 30px;
     background-color: #09747A;
+    
 }
 .side-btn.active .link-container::before {
     top: -60px;
+    
 }
 .side-btn.active .link-container::after {
     bottom: -60px;
     z-index: 99;
+    
 }
 .side-btn.active .link-container::before, .side-btn.active .link-container::after {
     position: absolute;
@@ -112,5 +118,6 @@ export default {
     width: 60px;
     border-radius: 50%;
     background-color: #10A0A7;
+    
 }
 </style>
