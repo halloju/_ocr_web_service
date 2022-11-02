6465d1ac7ba888fa3284a011db40a93f59e4a050
erik1110 <oceanuheart@gmail.com>
2022-11-02 11:17:49 +0800

    [style] 調整define、template的style

--- frontend/src/views/TemplatePrivate.vue
+++ frontend/src/views/TemplatePrivate.vue
@@ -1,72 +1,70 @@
 <template>
-  <div class="step">
-    Step.1 上傳範例圖片
-  </div>
-  <div class="buttons">
-    <button class="next-button">➡ 下一步</button>
-  </div>
-  <div class="contentWrapper py-5 px-4">
+  <div class="contentWrapper">
+  <div class="contentWrapper1 py-5 ">
     <div class="my-content">
-      Click your browser or <br>
-      drag and drop your files
     </div>
+  </div>
+  <div class="contentWrapper2 py-5">
+    <div class="my-content2">
+      檢視模板訊息
+    </div>
+  </div>
   </div>
 </template>
 
 <style scoped>
-.step {
-  margin-top: 3vh;
-  margin-left: 3vw;
-  color: white;
-  font-size: 21px;
-  font-weight: bold;
-}
-.buttons {
-  text-align: right;
-  margin-right: 1vw;
-}
-.buttons .next-button{
-  background-color: #FF9B26;
-  padding: 5px 20px;
-  border-radius: 1vw 1vw 1vw 1vw;
-  text-align: right;
-  color: white;
-  font-size: 15px;
-  font-weight: bold;
-}
-.contentWrapper{
-  display: block;
-  background-color: rgba(226, 230, 234, 0.3);
-  max-width: 95%;
+.contentWrapper {
+  display: flex;
   height: 80vh;
+  justify-content: center;
+}
+.contentWrapper1{
+  flex-grow:4;
+  background-color: rgba(226, 230, 234, 0.3);
+  height: 90vh;
   margin-top: 1.5vh;
   margin-bottom: 1.5vh;
   margin-left: 2vw;
   margin-right: 1vw;
   border-radius: 3vw 3vw 3vw 3vw;
 }
-.bar {
-  margin-left: 25vw;
-  margin-bottom: 2vh;
-}
-.bar button {
-  background-color: black;
+.contentWrapper2{
+  flex-grow:1;
+  background-color: rgba(226, 230, 234, 0.3);
+  height: 90vh;
+  margin-top: 1.5vh;
+  margin-bottom: 1.5vh;
+  margin-left: 1vw;
+  margin-right: 2vw;
   border-radius: 3vw 3vw 3vw 3vw;
-  border: 5px solid white;
+}
+.bar {
+  background-color: white;
+  margin-bottom: 20px;
+  text-align: left;
+}
+.bar .content-title {
+  margin-left: 30px;
+  color: #008b8b;
+  font-size: 17px;
   font-weight: bold;
-  text-align: center;
-
+}
+.bar .content-subtitle {
+  color: grey;
+  font-size: 17px;
 }
 .my-content {
   width: 100%;
-  height: 70vh;
-  padding: 0 0 0 0;
+  height: 75vh;
+  color: #008b8b;
+}
+.my-content2 {
+  position: relative;
+  text-align: center;
+  top: 50%;
+  width: 100%;
+  height: 80vh;
   color: #E2E6EA;
-  font-size: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center; 
-
 }
 </style>
 <script>
