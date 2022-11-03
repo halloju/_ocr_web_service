99e8d799d9cf43e9bf2ce6c858ea9d830ea1453a
erik1110 <oceanuheart@gmail.com>
2022-11-03 19:12:36 +0800

    [refactor] 引入 css

--- frontend/src/views/TemplatePrivate.vue
+++ frontend/src/views/TemplatePrivate.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="contentWrapper">
-  <div class="contentWrapper1 py-5 ">
-    <div class="my-content">
+  <div class="contentWrapperFlex">
+  <div class="contentWrapper3 py-5 ">
+    <div class="my-content0">
     </div>
   </div>
-  <div class="contentWrapper2 py-5">
-    <div class="my-content2">
+  <div class="contentWrapper4 py-5">
+    <div class="my-content3">
       檢視模板訊息
     </div>
   </div>
@@ -13,60 +13,7 @@
 </template>
 
 <style scoped>
-.contentWrapper {
-  display: flex;
-  height: 80vh;
-  justify-content: center;
-}
-.contentWrapper1{
-  flex-grow:4;
-  background-color: rgba(226, 230, 234, 0.3);
-  height: 90vh;
-  margin-top: 1.5vh;
-  margin-bottom: 1.5vh;
-  margin-left: 2vw;
-  margin-right: 1vw;
-  border-radius: 3vw 3vw 3vw 3vw;
-}
-.contentWrapper2{
-  flex-grow:1;
-  background-color: rgba(226, 230, 234, 0.3);
-  height: 90vh;
-  margin-top: 1.5vh;
-  margin-bottom: 1.5vh;
-  margin-left: 1vw;
-  margin-right: 2vw;
-  border-radius: 3vw 3vw 3vw 3vw;
-}
-.bar {
-  background-color: white;
-  margin-bottom: 20px;
-  text-align: left;
-}
-.bar .content-title {
-  margin-left: 30px;
-  color: #008b8b;
-  font-size: 17px;
-  font-weight: bold;
-}
-.bar .content-subtitle {
-  color: grey;
-  font-size: 17px;
-}
-.my-content {
-  width: 100%;
-  height: 75vh;
-  color: #008b8b;
-}
-.my-content2 {
-  display: flex;
-  justify-content: center;
-  align-items: center; 
-  font-size: 25px;
-  width: 100%;
-  height: 80vh;
-  color: #E2E6EA;
-}
+@import '@/assets/css/content.css';
 </style>
 <script>
 export default {
