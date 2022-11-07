a5685025fe682ced8d48522e3bd318c26d8df733
erik1110 <oceanuheart@gmail.com>
2022-11-07 11:11:53 +0800

    [feat] change rwd of sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -37,7 +37,7 @@ export default {
             if (parseInt(this.$route.name.slice(-1))!=='NaN') {
                 return !(item.order <= parseInt(this.$route.name.slice(-1)))
             } else {
-                return False
+                return
             }
         }
     }
@@ -45,112 +45,5 @@ export default {
 </script>
 
 <style scoped>
-
-.sidebarImg{
-    text-align: center;
-}
-.sidebarImg img{
-    margin-top: 3vh;
-    width: 30%;
-}
-.disabled {
-  pointer-events: none;
-}
-.title {
-    color: white;
-    font-size: 1.8rem;
-    margin-top: 3vh;
-    text-align: center;
-}
-.menu-items {
-    display: flex;
-    flex-direction: column;
-    margin-top: 5vh;
-}
-.menu-items .link-container p {
-    color: white;
-}
-.menu-items .side-btn {
-    min-width: 300px;
-    text-align: left;
-    border: none;
-    height: 100px;
-    width: 20vw;
-    padding: 2.3vh 5vw;
-    cursor: pointer;
-    font-size: 1.4rem;
-    font-weight: 500;
-    color: white;
-    background-color: transparent;
-    z-index: 100;
-}
-.side-btn:focus {
-    outline: none;
-    color: white;
-}
-.side-btn .link-container {
-    margin-top: 1vh;
-    color: white;
-    width: 300px;
-}
-.side-btn.active {
-    position: relative;
-    background-color: #09747A;
-    color: #10A0A7;
-    font-weight: bold;
-    margin-left: 0.7vw;
-    border-radius: 50px 0 0 50px;
-    z-index: 0;
-}
-.side-btn.active::before {
-    top: -30px;
-    
-}
-.side-btn.active::after {
-    bottom: -30px;
-    
-}
-.side-btn.active::before, .side-btn.active::after {
-    position: absolute;
-    content: "";
-    right: 0;
-    height: 30px;
-    width: 30px;
-    background-color: #09747A;
-    
-}
-.side-btn.active .link-container::before {
-    top: -60px;
-    
-}
-.side-btn.active .link-container::after {
-    bottom: -60px;
-    z-index: 99;
-    
-}
-.side-btn.active .link-container::before, .side-btn.active .link-container::after {
-    position: absolute;
-    content: "";
-    right: 0px;
-    height: 60px;
-    width: 60px;
-    border-radius: 50%;
-    background-color: #10A0A7;
-    
-}
-@media (min-width: 2000px) {
-.side-btn {
-    text-align: left;
-    border: none;
-    height: 100px;
-    width: 17vw;
-    padding: 1.5vh 5vw;
-    cursor: pointer;
-    font-size: 25px;
-    font-weight: 500;
-    color: white;
-    background-color: transparent;
-    z-index: 100;
-    }
-}
+@import '@/assets/css/sidebar.css';
 </style>
