97ae0a2ae260ba9784566e62a1245de5ba3cdde5
erik1110 <oceanuheart@gmail.com>
2022-12-07 17:47:53 +0800

    [feat] update landing

--- web/src/views/pages/Landing.vue
+++ web/src/views/pages/Landing.vue
@@ -47,7 +47,7 @@ export default {
                             </a>
                         </li>
                         <li>
-                            <a @click="smoothScroll('#pricing')" class="flex m-0 md:ml-5 px-0 py-3 text-900 font-medium text-2xl line-height-3 p-ripple" v-ripple>
+                            <a @click="smoothScroll('#faq')" class="flex m-0 md:ml-5 px-0 py-3 text-900 font-medium text-2xl line-height-3 p-ripple" v-ripple>
                                 <span>常見問題</span>
                             </a>
                         </li>
@@ -228,8 +228,8 @@ export default {
 
             <div id="highlights" class="py-4 px-4 lg:px-8 mx-0 my-6 lg:mx-8">
                 <div class="text-center">
-                    <h2 class="text-900 font-normal mb-2">Powerful Everywhere</h2>
-                    <span class="text-600 text-2xl">Amet consectetur adipiscing elit...</span>
+                    <h2 class="text-900 font-normal mb-2">關於產品</h2>
+                    <span class="text-600 text-2xl">齊心齊力打造更完善、優良的服務體驗</span>
                 </div>
 
                 <div class="grid mt-8 pb-2 md:pb-8">
@@ -241,7 +241,7 @@ export default {
                         <div class="flex align-items-center justify-content-center bg-purple-200 align-self-center lg:align-self-end" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
                             <i class="pi pi-fw pi-mobile text-5xl text-purple-700"></i>
                         </div>
-                        <h2 class="line-height-1 text-900 text-4xl font-normal">Congue Quisque Egestas</h2>
+                        <h2 class="line-height-1 text-900 text-4xl font-normal">手機</h2>
                         <span class="text-700 text-2xl line-height-3 ml-0 md:ml-2" style="max-width: 650px"
                             >Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Sit amet mattis vulputate enim nulla aliquet.</span
                         >
@@ -253,7 +253,7 @@ export default {
                         <div class="flex align-items-center justify-content-center bg-yellow-200 align-self-center lg:align-self-start" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
                             <i class="pi pi-fw pi-desktop text-5xl text-yellow-700"></i>
                         </div>
-                        <h2 class="line-height-1 text-900 text-4xl font-normal">Celerisque Eu Ultrices</h2>
+                        <h2 class="line-height-1 text-900 text-4xl font-normal">桌機</h2>
                         <span class="text-700 text-2xl line-height-3 mr-0 md:mr-2" style="max-width: 650px"
                             >Adipiscing commodo elit at imperdiet dui. Viverra nibh cras pulvinar mattis nunc sed blandit libero. Suspendisse in est ante in. Mauris pharetra et ultrices neque ornare aenean euismod elementum nisi.</span
                         >
