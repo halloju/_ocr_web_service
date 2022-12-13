756370b16e26d444af46a6527cc3d930765e2a67
erik1110 <oceanuheart@gmail.com>
2022-12-13 17:56:23 +0800

    [feat] add upload

--- web/src/views/pages/features/general/step1.vue
+++ web/src/views/pages/features/general/step1.vue
@@ -18,50 +18,46 @@
         <div class="col-12 md:col-9">
             <div class="card">
                 <el-upload action="#" list-type="picture-card" :auto-upload="false">
-                <el-icon><Plus /></el-icon>
+                    <el-icon><Plus /></el-icon>
+                    <template #file="{ file }">
+                    <div>
+                        <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+                        <span class="el-upload-list__item-actions">
+                        <span
+                            class="el-upload-list__item-preview"
+                            @click="handlePictureCardPreview(file)"
+                        >
+                            <el-icon><zoom-in /></el-icon>
+                        </span>
+                        <span
+                            v-if="!disabled"
+                            class="el-upload-list__item-delete"
+                            @click="handleDownload(file)"
+                        >
+                            <el-icon><Download /></el-icon>
+                        </span>
+                        <span
+                            v-if="!disabled"
+                            class="el-upload-list__item-delete"
+                            @click="handleRemove(file)"
+                        >
+                            <el-icon><Delete /></el-icon>
+                        </span>
+                        </span>
+                    </div>
+                    </template>
+                </el-upload>
 
-                <template #file="{ file }">
-                <div>
-                    <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
-                    <span class="el-upload-list__item-actions">
-                    <span
-                        class="el-upload-list__item-preview"
-                        @click="handlePictureCardPreview(file)"
-                    >
-                        <el-icon><zoom-in /></el-icon>
-                    </span>
-                    <span
-                        v-if="!disabled"
-                        class="el-upload-list__item-delete"
-                        @click="handleDownload(file)"
-                    >
-                        <el-icon><Download /></el-icon>
-                    </span>
-                    <span
-                        v-if="!disabled"
-                        class="el-upload-list__item-delete"
-                        @click="handleRemove(file)"
-                    >
-                        <el-icon><Delete /></el-icon>
-                    </span>
-                    </span>
-                </div>
-                </template>
-            </el-upload>
-
-  <el-dialog v-model="dialogVisible">
-    <img w-full :src="dialogImageUrl" alt="Preview Image" />
-  </el-dialog>
+                <el-dialog v-model="dialogVisible">
+                    <img :src="dialogImageUrl" alt="Preview Image" style="width:400px"/>
+                </el-dialog>
             </div>
         </div>
 
         <div class="col-12 md:col-3">
             <div class="card">
                 <h5>選擇語言</h5>
-                <Dropdown v-model="dropdownValue" :options="dropdownValues" optionLabel="name" placeholder="Select" />
-
-                <h5>選擇模板</h5>
-                <Dropdown v-model="dropdownValue" :options="dropdownValues" optionLabel="name" placeholder="Select" />
+                <Dropdown v-model="selectedLang" :options="languages" optionLabel="name" placeholder="Select" />
 
                 <h5>是否使用高精準度模型(耗時較久)</h5>
                 <InputSwitch v-model="switchValue" />
@@ -73,14 +69,17 @@
     </div>
 </template>
 <script>
-import { Delete, Download, Plus, ZoomIn } from '@element-plus/icons-vue'
-import 'element-plus/es/components/icon/style/css'
 
 export default {
     components: {},
     name: 'General1',
     data() {
         return {
+            selectedLang: null,
+            languages: [
+                {name: '繁體中文', code: 'zhant'},
+                {name: '英語', code: 'english'},
+            ],
             nestedRouteItems: [
                 {
                     label: '圖檔上傳',
@@ -98,13 +97,30 @@ export default {
                 { label: '通用辨識', to: '#' },
                 { label: '圖檔上傳', to: '#' }
             ],
-            switchValue: false
+            switchValue: false,
+            idCardImgUrl: '',
+            idCardIsUpload: false,
+            idCardUploadPercentage: 0,
+            errorImgUrls: [],
+            dialogVisible: false,
         };
+    },
+    props: {
+    /*可以在使用组件的时候传入一个支持上传的图片格式的数组进来，不传默认default数组*/
+        supportType: {//支持上传文件的格式
+        default: () => ['image/jpeg', 'image/jpg', 'image/png'],
+        type: Array
+        }
     },
     methods: {
         submit() {
             this.$router.push({ path: '/features/general/step2' });
+        },
+        handlePictureCardPreview(file) {
+            this.dialogImageUrl = file.url
+            this.dialogVisible = true
         }
+
     }
 };
 </script>
