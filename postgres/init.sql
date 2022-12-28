5f5dbfc8ddfc3d38419fb14c9850a5fff5ca06d7
Li Li Cheng <d40614d@gmail.com>
2022-12-28 11:31:51 +0800

    [feature] table template_info add column is_public

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,11 +1,13 @@
 CREATE SCHEMA if_gp_ocr;
 
+
 CREATE TABLE IF NOT EXISTS if_gp_ocr.template_info(
     template_id VARCHAR(19) PRIMARY KEY,
     user_id VARCHAR(5) NOT NULL,
     template_name VARCHAR,
     bbox JSON[],
-    updated_at TIMESTAMP WITH TIME ZONE
+    updated_at TIMESTAMP WITH TIME ZONE,
+    is_public BOOLEAN NOT NULL
 );
 
 /*紀錄驗證碼辨識結果的表*/
