f80a58195ae24600c9066e3e51251709222bc7d2
erik1110 <oceanuheart@gmail.com>
2023-02-04 14:01:56 +0800

    [feat] 新增垂直的 annotation template

--- web/src/views/pages/features/model-list/person.vue
+++ web/src/views/pages/features/model-list/person.vue
@@ -1,6 +1,72 @@
+<template>
+    <div class="grid">
+        <div class="col-7">
+            <div class="card" style="height: 850px; overflow-y: scroll">
+                <h2>個人模板檢視</h2>
+                <el-table :data="tableData" style="width: 100%">
+                    <el-table-column :prop="item.prop" :label="item.label" v-for="(item, index) in tableHeader" :key="item.prop" :width="item.width">
+                        <template #default="scope">
+                            <div v-show="item.editable || scope.row.editable" class="editable-row">
+                                <template v-if="item.type === 'input'">
+                                    <el-input size="small" v-model="scope.row[item.prop]" :placeholder="`請輸入 ${item.label}`" @change="handleEdit(scope.$index, scope.row)" />
+                                </template>
+                                <template v-if="item.type === 'date'">
+                                    <el-date-picker v-model="scope.row[item.prop]" type="date" value-format="YYYY-MM-DD" :placeholder="`請輸入 ${item.label}`" @change="handleEdit(scope.$index, scope.row)" />
+                                </template>
+                            </div>
+                            <div v-show="!item.editable && !scope.row.editable" class="editable-row">
+                                <span class="editable-row-span">{{ scope.row[item.prop] }}</span>
+                            </div>
+                        </template>
+                    </el-table-column>
+                    <el-table-column label="操作" width="320px">
+                        <template #default="scope">
+                            <el-button v-show="!scope.row.editable" size="big" @click="scope.row.editable = true">編輯</el-button>
+                            <el-button v-show="scope.row.editable" size="small" type="success" @click="scope.row.editable = false">確認</el-button>
+                            <el-button size="big" type="info" @click="">檢視</el-button>
+                            <el-button size="big" type="danger" @click="handleDelete(scope.$index)">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="card" style="height: 850px; overflow-y: scroll">
+                <h5>請選擇模式</h5>
+                <div class="flex flex-column card-container">
+                    <div class="flex align-items-center justify-content-center h-4rem font-bold border-round m-2">
+                        <SelectButton v-model="myModel" :options="models" optionLabel="name" />
+                    </div>
+                    <div class="flex align-items-center justify-content-center h-4rem font-bold border-round m-4">
+                        <Button icon="pi pi-download" class="p-button-rounded p-button-info mr-2 mb-2" v-tooltip="'下載模板設定檔'" @click="" />
+                        <Button icon="pi pi-pencil" class="p-button-rounded p-button-info mr-2 mb-2" v-tooltip="'編輯模版'" @click="getAvailableTemplate" />
+                        <Button icon="pi pi-trash" class="p-button-rounded p-button-info mr-2 mb-2" v-tooltip="'刪除模板'" @click="" disabled="true" />
+                    </div>
+                    <div class="flex align-items-center justify-content-center h-100rem font-bold border-round m-2">
+                        <AnnotationVertical
+                            containerId="my-pic-annotation-output"
+                            :editMode="false"
+                            :imageSrc="imageSrc"
+                            :width="width"
+                            :height="height"
+                            dataCallback=""
+                            :initialData="getShapeData"
+                            :initialDataId="initialDataId"
+                            :justShow="true"
+                        ></AnnotationVertical>
+                    </div>
+                    <div class="flex align-items-center justify-content-center h-100rem font-bold border-round m-2">
+                        <!-- <BoxCard boxName="text" :boxTitle="myModel.name" /> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 <script>
+import axios from "axios";
 import PhotoService from '@/service/PhotoService';
-// import BoxCard from '@/components/BoxCard.vue';
+import AnnotationVertical from '@/components/AnnotationVertical.vue';
 
 const item = {
     name: '',
@@ -18,18 +84,77 @@ const header = {
 };
 export default {
     name: 'public',
-    components: {},
+    components: {
+        AnnotationVertical
+    },
     created() {
         this.galleriaService = new PhotoService();
     },
     mounted() {
         this.galleriaService.getImages().then((data) => (this.images = data));
     },
+    computed: {
+        getShapeData() {
+            let myShapes = []
+            let image_cv_id = JSON.stringify(this.$store.state.general_upload_res[0].image_cv_id);
+            let regData = this.$store.state.general_upload_res[0].ocr_results;
+            regData.forEach(function(element, index) {
+                var label = Object.values(element);
+                var points = Object.values(label[0]);
+                var myContent = label[1];
+                var label_x = points[0][0];
+                var label_y = points[0][1];
+                var label_width = points[1][0] - label_x ;
+                var label_height = points[2][1] - label_y ;
+                myShapes.push({
+                    type: 'rect',
+                    name: image_cv_id + index,
+                    fill: '#b0c4de',
+                    opacity: 0.5,
+                    stroke: '#0ff',
+                    draggable: true,
+                    strokeWidth: 2,
+                    strokeScaleEnabled: false,
+                    annotation: {
+                        title: index+1,
+                        text: myContent,
+                        linkTitle: '',
+                        link: ''
+                    },
+                    x: label_x,
+                    y: label_y,
+                    width: label_width,
+                    height: label_height
+                });
+            });
+            return JSON.stringify(myShapes)
+        },
+        // getAvailableTemplate() {
+        //     let user_id = '13520';
+        //     axios.get("/template/get_available_templates", {
+        //                         "user_id": user_id,
+        //                         "is_public": false,
+        //                     })
+        //                     .then( (response) =>
+        //                        {
+        //                         console.log(response)
+        //                         })
+        //                     .catch( (error) => {
+        //                         console.log(error)
+        //                         if(error.code === 'ERR_NETWORK'){
+        //                             this.status = 'network';
+        //                         }
+        //         })
+        // },
+    },
     data() {
         return {
             models: [{ name: '文字位置標註' }, { name: '方塊位置標註' }, { name: '遮罩位置標註' }],
             myModel: { name: '文字位置標註' },
+            width: 1000,
+            height: 500,
             images: null,
+            imageSrc: this.$store.state.general_upload_image[0].reader,
             tableHeader: [
                 {
                     prop: 'no',
@@ -78,6 +203,20 @@ export default {
         };
     },
     methods: {
+        getAvailableTemplate() {
+            let user_id = '13520';
+            axios.get("/template/get_available_templates/13520?is_public=false")
+                            .then( (response) =>
+                               {
+                                console.log(response)
+                                })
+                            .catch( (error) => {
+                                console.log(error)
+                                if(error.code === 'ERR_NETWORK'){
+                                    this.status = 'network';
+                                }
+                })
+        },
         handleEdit(row) {
             row.editable = true;
         },
@@ -102,58 +241,3 @@ export default {
     }
 };
 </script>
-<template>
-    <div class="grid">
-        <div class="col-7">
-            <div class="card" style="height: 850px; overflow-y: scroll">
-                <h2>個人模板檢視</h2>
-                <el-table :data="tableData" style="width: 100%">
-                    <el-table-column :prop="item.prop" :label="item.label" v-for="(item, index) in tableHeader" :key="item.prop" :width="item.width">
-                        <template #default="scope">
-                            <div v-show="item.editable || scope.row.editable" class="editable-row">
-                                <template v-if="item.type === 'input'">
-                                    <el-input size="small" v-model="scope.row[item.prop]" :placeholder="`請輸入 ${item.label}`" @change="handleEdit(scope.$index, scope.row)" />
-                                </template>
-                                <template v-if="item.type === 'date'">
-                                    <el-date-picker v-model="scope.row[item.prop]" type="date" value-format="YYYY-MM-DD" :placeholder="`請輸入 ${item.label}`" @change="handleEdit(scope.$index, scope.row)" />
-                                </template>
-                            </div>
-                            <div v-show="!item.editable && !scope.row.editable" class="editable-row">
-                                <span class="editable-row-span">{{ scope.row[item.prop] }}</span>
-                            </div>
-                        </template>
-                    </el-table-column>
-                    <el-table-column label="操作" width="320px">
-                        <template #default="scope">
-                            <el-button v-show="!scope.row.editable" size="big" @click="scope.row.editable = true">編輯</el-button>
-                            <el-button v-show="scope.row.editable" size="small" type="success" @click="scope.row.editable = false">確認</el-button>
-                            <el-button size="big" type="info" @click="">檢視</el-button>
-                            <el-button size="big" type="danger" @click="handleDelete(scope.$index)">删除</el-button>
-                        </template>
-                    </el-table-column>
-                </el-table>
-            </div>
-        </div>
-        <div class="col-5">
-            <div class="card" style="height: 850px; overflow-y: scroll">
-                <h5>請選擇模式</h5>
-                <div class="flex flex-column card-container">
-                    <div class="flex align-items-center justify-content-center h-4rem font-bold border-round m-2">
-                        <SelectButton v-model="myModel" :options="models" optionLabel="name" />
-                    </div>
-                    <div class="flex align-items-center justify-content-center h-4rem font-bold border-round m-4">
-                        <Button icon="pi pi-download" class="p-button-rounded p-button-info mr-2 mb-2" v-tooltip="'下載模板設定檔'" @click="" />
-                        <Button icon="pi pi-pencil" class="p-button-rounded p-button-info mr-2 mb-2" v-tooltip="'編輯模版'" @click="" />
-                        <Button icon="pi pi-trash" class="p-button-rounded p-button-info mr-2 mb-2" v-tooltip="'刪除模板'" @click="" disabled="true" />
-                    </div>
-                    <div class="flex align-items-center justify-content-center h-100rem font-bold border-round m-2">
-                        <Image src="../src/assets/img/card-visa.jpg" alt="Image" width="500" preview />
-                    </div>
-                    <div class="flex align-items-center justify-content-center h-100rem font-bold border-round m-2">
-                        <!-- <BoxCard boxName="text" :boxTitle="myModel.name" /> -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
