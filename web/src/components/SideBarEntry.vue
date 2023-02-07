fba1de5a58d78aa361aa385eff0911358f6d58d7
erik1110 <oceanuheart@gmail.com>
2023-02-07 15:27:13 +0800

    [style] 移除 SideBarEntry 的 icon 顯示

--- web/src/components/SideBarEntry.vue
+++ web/src/components/SideBarEntry.vue
@@ -3,7 +3,7 @@
        :class="{'is-selected-target': selectedShapeName === shape.name, 'is-hover-target': currentHoverShape === shape.name}">
 
     <button type="button" @click.prevent.stop="toggleContent" class="pa-accordion" :class="{'is-active': active}">
-      <icon :type="shape.type" />
+      <!-- <icon :type="shape.type" /> -->
       <span v-if="shape.annotation.title" class="pa-side-bar-title">{{shape.annotation.title}}.</span>
       <span v-if="editMode && (active || selectedShapeName === shape.name)" class="pa-side-bar-icons">
         <a href="#" @click.prevent="deleteShape" :title="delete_shape"><icon type="delete-shape" fill="red" /></a>
