ee7cb7f3321cfebd00392fc6487fd1ac40368add
708kelly <b03701221@ntu.edu.tw>
2023-02-17 17:46:04 +0800

    [update] update delete api to mlaas api

--- mlaas/app/routers/template_crud/delete.py
+++ mlaas/app/routers/template_crud/delete.py
@@ -8,12 +8,12 @@ from sqlalchemy.orm import Session
 from app.schema.template_crud.delete import DeleteTemplateRequest
 from app.forms.template_crud.delete import DeleteTemplateForm
 from app.services.template_crud import delete as service_delete
-
+from app.schema.template_crud.delete import DeleteTemplateResponse
 
 router = APIRouter()
 
 
-@router.post("/delete_template")
+@router.post("/delete_template", response_model=DeleteTemplateResponse)
 async def delete_template(request: DeleteTemplateRequest, db: Session = Depends(get_db)):
     '''
     將 Feature DB 中的 template 資訊刪除
@@ -25,6 +25,5 @@ async def delete_template(request: DeleteTemplateRequest, db: Session = Depends(
             template_id=form.template_id
             )
         template_id = service_delete.delete_template(template=template, db=db)
-        return Response(data={"status": 200})
-    raise CustomException(status_code=400, message=form.errors)
+        return DeleteTemplateResponse()
     raise CustomException(status_code=400, message=form.errors)
