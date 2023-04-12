27391ee1c08b912b4738008c7547f17cb052d619
halloju <wanchu.lin133@gmail.com>
2023-04-12 10:37:15 +0800

    [fix] change to last version

--- web/src/components/BaseOcrResultShow.vue
+++ web/src/components/BaseOcrResultShow.vue
@@ -191,8 +191,8 @@ export default {
         function getShapeData(regData) {
             let myShapes = [];
             // 使用正規表達式將 regData 中的所有 ' 符號替換為 " 符號
-            regData = JSON.parse(JSON.stringify(regData));
-            
+            regData = JSON.parse(regData.replace(/'/g, '"'));
+
             regData.forEach(function (element, index) {
                 var label = Object.values(element);
                 var points = Object.values(label[0]);
