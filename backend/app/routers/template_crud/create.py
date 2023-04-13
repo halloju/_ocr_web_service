804509cc899d480ef6d58921d449732127c244f7
708kelly <b03701221@ntu.edu.tw>
2023-04-13 17:55:30 +0800

    [features] add create template log

--- backend/app/routers/template_crud/create.py
+++ backend/app/routers/template_crud/create.py
@@ -1,18 +1,18 @@
-from app.exceptions import CustomException
-from app.schema.common import Response
-from fastapi import APIRouter
-from fastapi.encoders import jsonable_encoder
-from fastapi import Depends
-
-from app.schema.template_crud.create import CreateTemplateRequest
-from app.forms.template_crud.create import CreateTemplateForm
-from app.schema.template_crud.create import CreateTemplateResponse
-from route_utils import get_user_id, call_mlaas_function, get_request_id
-from app.exceptions import MlaasRequestError
-from app import response_table
 import json
 
+from app import response_table
+from app.exceptions import CustomException, MlaasRequestError
+from app.forms.template_crud.create import CreateTemplateForm
+from app.schema.common import Response
+from app.schema.template_crud.create import (CreateTemplateRequest,
+                                             CreateTemplateResponse)
+from fastapi import APIRouter, Depends
+from fastapi.encoders import jsonable_encoder
+from logger import Logger
+from route_utils import call_mlaas_function, get_user_id, get_request_id
+
 router = APIRouter()
+logger = Logger('template_create')
 
 
 @router.post("/create_template", response_model=CreateTemplateResponse)  # responses={},
@@ -20,33 +20,41 @@ async def create_template(request: CreateTemplateRequest):
     '''
     將 template 影像上傳至 MinIO, 並將其餘資訊存入 Feature DB
     '''
+    log_main = {'user_id': uid, 'request_id': rid, 'action': action}
     form = CreateTemplateForm(request)
     await form.load_data()
     if await form.is_valid():
+        uid = get_user_id()
+        rid = get_request_id()
+        action = 'template_crud/create_template'
         inputs = {
-            'user_id': get_user_id(),
+            'user_id': uid,
             'image': form.image,
             'points_list': form.points_list,
             'template_name': form.template_name
         }
         input_data = {
             "business_unit": "C170",
-            "request_id": "111",
+            "request_id": rid,
             "inputs": jsonable_encoder(inputs)
         }
-        outputs = call_mlaas_function(input_data, 'template_crud/create_template', project='GP', timeout=60)
+        logger.info(log_main)
+        outputs = call_mlaas_function(input_data, action, project='GP', logger=logger, timeout=60)
         status_code = outputs['outputs']['status_code']
         if status_code == '0000':
-            print(outputs['outputs']['template_id'])
             return CreateTemplateResponse(
                 template_id=outputs['outputs']['template_id'],
                 status_code='0000',
                 status_msg='OK'
             )
         elif status_code == '5401':
+            logger.error({**log_main, 'error_msg': response_table.status_uniqueviolation})
             raise MlaasRequestError(**response_table.status_uniqueviolation)
         elif status_code == '5402':
+            logger.error({**log_main, 'error_msg': response_table.status_image_type_error})
             raise MlaasRequestError(**response_table.status_image_type_error)
         else:
+            logger.error({**log_main, 'error_msg': outputs['outputs']})
             raise MlaasRequestError(status_code, outputs['outputs']['status_msg'])
+    logger.error({**log_main, 'error_msg': {'form is not valid': {'errors': form.errors}}})
     raise CustomException(status_code=400, message=form.errors)
