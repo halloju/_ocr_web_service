80f6e871de62431a34179e7d48a94faa99f6a1f4
708kelly <b03701221@ntu.edu.tw>
2023-04-13 16:53:33 +0800

    [features] add log into worker and call_mlaas_func

--- backend/route_utils.py
+++ backend/route_utils.py
@@ -1,13 +1,13 @@
-import requests
 import json
-from fastapi import Request
-from app.exceptions import MlaasRequestError
 import os
 
+import requests
+from app.exceptions import MlaasRequestError
 
-def call_mlaas_function(request, action: str, project, timeout=5):
+
+def call_mlaas_function(request, action: str, project, logger, timeout=5):
+    logger.info({'call_mlaas_function': 'start'})
     mlaas_url = os.environ.get(f'{project}_MLAAS_URL')
-    
     if os.environ.get('MODE') == 'dev':
         connection_url = f'{mlaas_url}/{action}'
         inp_post_response = requests.post(connection_url, json=request)
@@ -19,10 +19,21 @@ def call_mlaas_function(request, action: str, project, timeout=5):
             'Authorization': os.environ.get(f'{project}_MLAAS_JWT'),
             'Content-Type': 'application/json'
         }
-        inp_post_response = requests.post(connection_url+'/v1', json=request, headers=headers, timeout=timeout, verify=False)
+        inp_post_response = requests.post(f'{connection_url}/v1', json=request, headers=headers, timeout=timeout, verify=False)
+    logger.info({'call_mlaas_function': {
+        'status_code': inp_post_response.status_code,
+        'connection_url': connection_url,
+        'request_input_keys': list(request['inputs'].keys())
+    }})
+    logger.debug({
+        'call_mlaas_function': {'response': inp_post_response.json()}
+    })
     if inp_post_response.status_code == 200:
         return json.loads(inp_post_response.content.decode('utf-8'))
-    raise MlaasRequestError()
+    logger.error({'call_mlaas_function': {
+        'error_msg': inp_post_response.text
+    }})
+    raise MlaasRequestError(inp_post_response.status_code, inp_post_response.text)
 
 def get_user_id() -> str:
     return '13520'
--- backend/worker.py
+++ backend/worker.py
@@ -1,16 +1,24 @@
+import json
 import logging
 import os
-import json
-import requests
 import uuid
-from celery import Celery
-from celery import Task
 
-from route_utils import get_request_id, call_mlaas_function
+import requests
+from celery import Celery, Task
+from celery.signals import after_setup_logger
+from logger import config_logging
+from route_utils import call_mlaas_function, get_request_id
 
+# 設定 celery
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://rabbitmq")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+
+# 設定 logger
+logger = logging.getLogger(__name__)
+@after_setup_logger.connect
+def config_loggers(logger, *args, **kwags):
+    config_logging()
 
 class PredictTask(Task):
     def __init__(self):
@@ -22,7 +30,7 @@ class PredictTask(Task):
             'template_ocr': 'GP',
             'check_front': 'CHECK',  # 支票正面
             'check_back': 'CHECK',  # 支票背面
-            'remittance': 'REMIT' # 匯款單
+            'remittance': 'REMIT'  # 匯款單
         }
         self.endpoints = {
             'gp_ocr': 'ocr/gp_ocr',
@@ -30,9 +38,8 @@ class PredictTask(Task):
             'check_front': 'ocr/front_out_predict',  # 支票正面
             'check_back': 'ocr/back_predict',  # 支票背面
             'remittance': 'ocr/predict' # 匯款單
-            
         }
-        
+
     def __call__(self, *args, **kwargs):
         return self.run(*args, **kwargs)
 
@@ -47,25 +54,34 @@ class PredictTask(Task):
                     **input_params
                 }
             }
-            data_pred = call_mlaas_function(input_data, action=self.endpoints[action], project= self.project_names[action], timeout=60)
+            data_pred = call_mlaas_function(
+                input_data,
+                action=self.endpoints[action],
+                project=self.project_names[action],
+                logger=logger,
+                timeout=60
+            )
+
+            logger.debug({'predict': {'image_id': image_id, 'data_pred': list(data_pred.keys())}})
             if action in ['check_front', 'check_back', 'remittance']:
                 if data_pred['outputs']['status_code'] == '0000':
                     data_pred['outputs']['data_results'] = [{'tag': key, 'text': value} for key, value in data_pred['outputs'].items() if key not in ['status_code', 'status_msg', 'err_detail']]
             # Return the prediction result
             return data_pred
-        except Exception as ex:
-            logging.error(ex)
-            raise ex
+        except Exception as e:
+            logger.error({'predict': {'error_msg': str(e), 'image_id': image_id, 'action': action, 'input+params': input_params}})
+            raise e
 
 @celery.task(name="upload_to_redis")
 def upload_to_redis(image_data):
+    image_id = str(uuid.uuid4())
     try:
-        image_id = str(uuid.uuid4())
         # Store image data in Redis with 1-day expiration
         celery.backend.set(image_id, image_data)
         celery.backend.expire(image_id, 86400)
         return {'status': 'SUCCESS', 'image_id': image_id}
-    except:
+    except Exception as e:
+        logger.error({'upload_to_redis': {'error_msg': str(e), 'image_id': image_id}})
         return {'status': 'FAIL'}
 
 @celery.task(name="get_from_redis")
@@ -74,10 +90,12 @@ def get_from_redis(task_id):
         full_key = f'celery-task-meta-{task_id}'
         task_result = celery.backend.get(full_key)
         if task_result is None:
+            logger.error({'get_from_redis': f'task_result of id {task_id} is None'})
             return {'status': 'ERROR'}
         task_result_dict = json.loads(task_result.decode('utf-8'))
         return task_result_dict["result"]
-    except:
+    except Exception as e:
+        logger.error({'get_from_redis': {'error_msg': str(e), 'task_id': task_id}})
         return {'status': 'FAIL'}
 
 @celery.task(ignore_result=False, bind=True, base=PredictTask)
@@ -92,6 +110,5 @@ def predict_image(self, image_id, action, input_params):
         file_name = celery.backend.get(image_id + '_file_name').decode("utf-8")
 
         return {'status': status, 'result': data_pred, 'file_name': file_name}
-
-    except Exception as ex:
-        logging.error(ex)
+    except Exception as e:
+        logger.error({'predict_image': {'error_msg': str(e), 'image_id': image_id, 'action': action}})
