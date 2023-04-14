ea51cfb21d8dafc755951a274efcd9e4ef1bba66
708kelly <b03701221@ntu.edu.tw>
2023-04-14 10:34:21 +0800

    [refactor] refactor template crud function (log)

--- backend/app/routers/template_crud/create.py
+++ backend/app/routers/template_crud/create.py
@@ -1,32 +1,26 @@
-import json
-
 from app import response_table
 from app.exceptions import CustomException, MlaasRequestError
 from app.forms.template_crud.create import CreateTemplateForm
-from app.schema.common import Response
 from app.schema.template_crud.create import (CreateTemplateRequest,
                                              CreateTemplateResponse)
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 from logger import Logger
-from route_utils import call_mlaas_function, get_user_id, get_request_id
+from route_utils import call_mlaas_function, init_log
 
 router = APIRouter()
-logger = Logger('template_create')
+logger = Logger(__name__)
 
 
 @router.post("/create_template", response_model=CreateTemplateResponse)  # responses={},
 async def create_template(request: CreateTemplateRequest):
     '''
-    將 template 影像上傳至 MinIO, 並將其餘資訊存入 Feature DB
+    將前端標注的點位和上傳的圖片傳至 mlaas 對應的 api
     '''
-    log_main = {'user_id': uid, 'request_id': rid, 'action': action}
     form = CreateTemplateForm(request)
     await form.load_data()
     if await form.is_valid():
-        uid = get_user_id()
-        rid = get_request_id()
-        action = 'template_crud/create_template'
+        uid, rid, log_main = init_log('template_create', logger)
         inputs = {
             'user_id': uid,
             'image': form.image,
@@ -38,8 +32,8 @@ async def create_template(request: CreateTemplateRequest):
             "request_id": rid,
             "inputs": jsonable_encoder(inputs)
         }
-        logger.info(log_main)
-        outputs = call_mlaas_function(input_data, action, project='GP', logger=logger, timeout=60)
+
+        outputs = call_mlaas_function(input_data, 'template_ocr/create_template', project='GP', logger=logger, timeout=60)
         status_code = outputs['outputs']['status_code']
         if status_code == '0000':
             return CreateTemplateResponse(
--- backend/app/routers/template_crud/read.py
+++ backend/app/routers/template_crud/read.py
@@ -1,16 +1,13 @@
-from app.exceptions import CustomException
-from app.schema.common import Response
-from fastapi import APIRouter
-from fastapi import Depends
-
-# from app.services.template_crud import read as service_read
-from app.schema.template_crud.read import GetAvailableTemplatesResponse
-from app.schema.template_crud.read import GetTemplateDetailResponse
-from route_utils import get_user_id, call_mlaas_function, get_request_id
-from app.exceptions import MlaasRequestError
 from app import response_table
+from app.exceptions import MlaasRequestError
+from app.schema.template_crud.read import (GetAvailableTemplatesResponse,
+                                           GetTemplateDetailResponse)
+from fastapi import APIRouter
+from logger import Logger
+from route_utils import call_mlaas_function, init_log
 
 router = APIRouter()
+logger = Logger(__name__)
 
 
 @router.get("/get_available_templates/{user_id}", response_model=GetAvailableTemplatesResponse)
@@ -18,19 +15,21 @@ def get_available_templates(user_id: str):
     '''
     取得該 user_id 可用的 template 清單
     '''
+    uid, rid, log_main = init_log('get_available_templates', logger, uid=user_id)
     input_data = {
         "business_unit": "C170",
-        "request_id": get_request_id(),
-        "inputs": {'user_id': user_id}
+        "request_id": rid,
+        "inputs": {'user_id': uid}
     }
-    outputs = call_mlaas_function(input_data, 'template_crud/get_available_templates', project='GP')
+    outputs = call_mlaas_function(input_data, 'template_crud/get_available_templates', project='GP', logger=logger)
     status_code = outputs['outputs']['status_code']
     if status_code == '0000':
         return GetAvailableTemplatesResponse(
             template_infos=outputs['outputs']['template_infos']
         )
     else:
-        raise MlaasRequestError(**response_table.status_mlaaserror)
+        logger.error({**log_main, 'error_msg': outputs['outputs']})
+        raise MlaasRequestError(status_code, outputs['outputs']['status_msg'])
 
 
 @router.get("/get_template_detail/{template_id}", response_model=GetTemplateDetailResponse)
@@ -38,12 +37,13 @@ def get_template_detail(template_id: str):
     '''
     取得該 template 的細節
     '''
+    uid, rid, log_main = init_log('get_template_detail', logger)
     input_data = {
         "business_unit": "C170",
-        "request_id": get_request_id(),
+        "request_id": rid,
         "inputs": {'template_id': template_id}
     }
-    outputs = call_mlaas_function(input_data, 'template_crud/get_template_detail', project='GP')
+    outputs = call_mlaas_function(input_data, 'template_crud/get_template_detail', project='GP', logger=logger)
     status_code = outputs['outputs']['status_code']
     if status_code == '0000':
         template_detail = outputs['outputs']['template_detail']
@@ -56,6 +56,8 @@ def get_template_detail(template_id: str):
             expiration_time=template_detail['expiration_time']
         )
     elif status_code == '5407':
+        logger.error({**log_main, 'error_msg': response_table.status_templateexisterror})
         raise MlaasRequestError(**response_table.status_templateexisterror)
     else:
+        logger.error({**log_main, 'error_msg': outputs['outputs']})
         raise MlaasRequestError(status_code, outputs['outputs']['status_msg'])
