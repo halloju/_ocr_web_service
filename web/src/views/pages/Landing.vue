22de866c0d91a861b5a998d7043ed48be9fcbcda
708kelly <b03701221@ntu.edu.tw>
2023-04-26 17:53:20 +0800

    [clean] rm unsue vue

--- web/src/views/pages/Landing.vue
+++ web/src/views/pages/Landing.vue
@@ -217,12 +217,11 @@ export default {
                         style="border-radius: 20px; background: linear-gradient(0deg, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, #efe1af 0%, #c3dcfa 100%)"
                     >
                         <div class="flex flex-column justify-content-center align-items-center text-center px-3 py-3 md:py-0">
-                            <h3 class="text-gray-900 mb-2">Joséphine Miller</h3>
-                            <span class="text-gray-600 text-2xl">Peak Interactive</span>
+                            <h3 class="text-gray-900 mb-2">=v=</h3>
+                            <span class="text-gray-600 text-2xl">A_A</span>
                             <p class="text-gray-900 sm:line-height-2 md:line-height-4 text-2xl mt-4" style="max-width: 800px">
-                                “Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.”
+                                “Be Happy.”
                             </p>
-                            <img src="/demo/images/landing/peak-logo.svg" class="mt-4" alt="Company logo" />
                         </div>
                     </div>
                 </div>
