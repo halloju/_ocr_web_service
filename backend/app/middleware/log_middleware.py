af2f93f6f4321b5d5887ee5bca976ea821e173d1
708kelly <b03701221@ntu.edu.tw>
2023-07-04 11:55:05 +0800

    [backend] add timeit

--- backend/app/middleware/log_middleware.py
+++ backend/app/middleware/log_middleware.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 from app.logger.logger import Logger
@@ -6,7 +7,7 @@ from starlette.requests import Request
 
 
 def get_request_id() -> str:  # celery with task_id
-    return datetime.now().strftime("%Y/%m/%d/%H/%M/%S/") + 'gpocr_system_test'
+    return datetime.now().strftime("%Y/%m/%d/%H/%M/%S/") + 'gpocr_web'
 
 class LogMiddleware(BaseHTTPMiddleware):
     def __init__(
@@ -23,13 +24,14 @@ class LogMiddleware(BaseHTTPMiddleware):
         # uid = getattr(request.state, "user_id", None)
         rid = get_request_id()
         request.state.request_id = rid
-        request.state.logger = Logger(__name__, uid, rid)
+        request.state.logger = Logger('backend', uid, rid)
         # Log before processing
         request.state.logger.debug(f"Request {request.method} {request.url} received")
-
+        start = time.time()
         # Call the next middleware or route handler
         response = await call_next(request)
-
+        if not request.url.path.startswith('/task'):
+            request.state.logger.info({request.url.path: {'response_time': time.time() - start}})
         # Log after processing
         request.state.logger.debug(f"Response status: {response.status_code}")
 
