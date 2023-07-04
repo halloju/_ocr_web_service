bcc9d5f316cdfd59245541a906e7231361a116ae
708kelly <b03701221@ntu.edu.tw>
2023-07-04 11:35:49 +0800

    [flake8] fix log middleware

--- backend/app/middleware/log_middleware.py
+++ backend/app/middleware/log_middleware.py
@@ -1,11 +1,12 @@
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
-from app.logger.logger import Logger
 from datetime import datetime
 
-def get_request_id() -> str: # celery with task_id
+from app.logger.logger import Logger
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+
+
+def get_request_id() -> str:  # celery with task_id
     return datetime.now().strftime("%Y/%m/%d/%H/%M/%S/") + 'gpocr_system_test'
-  
 
 class LogMiddleware(BaseHTTPMiddleware):
     def __init__(
@@ -13,17 +14,16 @@ class LogMiddleware(BaseHTTPMiddleware):
             app
     ):
         super().__init__(app)
+
     async def dispatch(self, request: Request, call_next):
         bypass_auth_paths = ["/docs", "/docs/oauth2-redirect", "/redoc", "/openapi.json", "/auth/is_authenticated", "/auth/refresh_token"]
         if any(request.url.path.startswith(path) for path in bypass_auth_paths):
             return await call_next(request)
-        
         uid = request.state.user_id
-        #uid = getattr(request.state, "user_id", None)
+        # uid = getattr(request.state, "user_id", None)
         rid = get_request_id()
         request.state.request_id = rid
         request.state.logger = Logger(__name__, uid, rid)
-        
         # Log before processing
         request.state.logger.debug(f"Request {request.method} {request.url} received")
 
@@ -35,4 +35,3 @@ class LogMiddleware(BaseHTTPMiddleware):
 
         # Return the response
         return response
-
