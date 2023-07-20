f011b2ac5dcdade2a466f7ca1ebb095aff99a627
708kelly <b03701221@ntu.edu.tw>
2023-07-20 18:14:45 +0800

    [web] add min_size constant

--- web/src/constants.js
+++ web/src/constants.js
@@ -2,6 +2,7 @@ export const FILE_SIZE_LIMIT = 5 * 1024 * 1024; // 5 MB
 export const API_TIMEOUT = 600000; // 5 minutes (in milliseconds)
 export const PULL_INTERVAL = 3000; // xx second (in milliseconds)
 export const MAX_RETRIES = 30; // 30 times
+export const MIN_PIXEL = 100; // create template
 export const default_error_msg = '系統非預期錯誤，請聯絡管理員';
 export const error_table = {
     // '0001': '系統非預期錯誤，請聯絡管理員',
@@ -12,7 +13,8 @@ export const error_table = {
     '5413': '圖檔過大，請縮小影像尺寸再次上傳',
     // '5415': '系統非預期錯誤，請聯絡管理員',
     // '5416': '系統非預期錯誤，請聯絡管理員',
-    '5001': '網頁系統非預期錯誤，請聯絡管理員', // web unknown error
-    '5002': '網頁系統非預期錯誤，請聯絡管理員', // web redis unknown error
-    '5003': '網頁系統非預期錯誤，請聯絡管理員', // web redis result find error
+    // '5001': '網頁系統非預期錯誤，請聯絡管理員', // web unknown error
+    // '5002': '網頁系統非預期錯誤，請聯絡管理員', // web redis unknown error
+    // '5003': '網頁系統非預期錯誤，請聯絡管理員', // web redis result find error
+    '5004': '要求逾時，請重新上傳圖片'
 };
