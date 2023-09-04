fb936cb6176bd085e4c0cac781afe07dca23009b
halloju <wanchu.lin133@gmail.com>
2023-09-04 18:55:05 +0800

    [fix]  mask 不需要欄位名稱 & 名稱綁 id

--- web/src/components/SideBarEntry.vue
+++ web/src/components/SideBarEntry.vue
@@ -40,14 +40,22 @@ export default {
             // form data as copy
             formData: [],
             buttonText: this.editMode ? '欄位命名' : '欄位',
-            buttonTitle: this.justShow ? '區塊類型' : 'index',
+            buttonTitle: this.justShow ? '區塊類型' : 'index'
             // shouldBeDisabled: false
         };
     },
     created() {
         console.log(this.shapes);
         if (this.shapes) {
-            this.formData = this.shapes
+            this.formData = this.shapes;
+        }
+    },
+    computed: {
+        showFormColumn() {
+            return this.rectangleType !== 'mask';
+        },
+        showSaveButton() {
+            return (this.rectangleType !== 'mask' && this.editMode) || !this.justShow;
         }
     },
     methods: {
@@ -63,11 +71,13 @@ export default {
         submitted(shape) {
             // copy back data
             // find the shape index
-            const idx = this.shapes.map((shape, index) => {
-                if (shape.name === shape.name) {
-                    return index;
-                }
-            }).filter(item => item !== undefined);
+            const idx = this.shapes
+                .map((shape, index) => {
+                    if (shape.name === shape.name) {
+                        return index;
+                    }
+                })
+                .filter((item) => item !== undefined);
             this.$emit('sidebar-entry-save', shape.name);
         },
         truncateText(text, maxLength) {
@@ -91,75 +101,94 @@ export default {
             console.log(index);
             this.formData[index].edited = true;
         },
+        inputClass(row) {
+            return {
+                'disabled-input': !row.edited
+            };
+        },
+        inputValue(row) {
+            if (this.editMode || this.justShow) {
+                return row.annotation.title;
+            } else if (row.annotation.text !== undefined) {
+                return row.annotation.text;
+            }
+            return '';
+        },
+        handleInput(event, row) {
+            if (this.editMode || this.justShow) {
+                row.annotation.title = event.target.value;
+            } else if (row.annotation.text !== undefined) {
+                row.annotation.text = event.target.value;
+            }
+        }
     },
     watch: {
         selectedShapeName: function (newShape, oldShape) {
             // find index of new shape
-            const idx = this.shapes.map((shape, index) => {
-                if (shape.name === newShape) {
-                    return index;
-                }
-            }).filter(item => item !== undefined);
-            this.selectRow(this.formData[idx])
+            const idx = this.shapes
+                .map((shape, index) => {
+                    if (shape.name === newShape) {
+                        return index;
+                    }
+                })
+                .filter((item) => item !== undefined);
+            this.selectRow(this.formData[idx]);
         },
         shapes() {
-            this.formData = this.shapes
+            this.formData = this.shapes;
         }
     }
 };
 </script>
 
 <template>
-    <div>
-        <div class="flex align-items-center justify-content-center font-bold m-2 mb-5">
-            <el-table ref='myTable' :data="formData" style="width: 100%" :row-key="selectedShapeName" border @cell-mouse-enter="handleMouseEnter" @cell-mouse-leave="handleMouseLeave" highlight-current-row>
-                
-                <!-- Annotation Title Column -->
-                <el-table-column prop="annotation.title" :label="buttonTitle" :min-width="20">
-                    <template v-slot="scope">
-                        <span v-if="!justShow" class="font-bold">{{ scope.$index }}.</span>
-                        <span v-else>{{ scope.row.rectangleType }}.</span>
-                    </template>
-                </el-table-column>
-                
-                <!-- Form Column -->
-                <el-table-column :label="buttonText" :min-width="50">
-                    <template v-slot="scope">
-                        <!--  模板編輯 -->
-                        <el-input v-if="editMode && rectangleType != 'mask'" :class="{ 'disabled-input': !scope.row.edited }" v-model="scope.row.annotation.title" @click="handleClick(scope.$index)" >{{ scope.row.annotation.title }}</el-input>
-                        <!--  模板編輯確認 -->
-                        <el-input v-else-if="!editMode && justShow" v-model="scope.row.annotation.title" disabled >{{ scope.row.annotation.title }}</el-input>
-                        <!--  辨認結果 -->
-                        <el-input v-else-if="scope.row.annotation.text != undefined" :class="{ 'disabled-input': !scope.row.edited }" v-model="scope.row.annotation.text" @click="handleClick(scope.$index)" >{{ scope.row.annotation.text }}</el-input>
-                    </template>
-                </el-table-column>
+    <div class="table-container">
+        <el-table ref="myTable" :data="formData" style="width: 100%" :row-key="selectedShapeName" border @cell-mouse-enter="handleMouseEnter" @cell-mouse-leave="handleMouseLeave" highlight-current-row>
+            <!-- Annotation Title Column -->
+            <el-table-column prop="annotation.title" :label="buttonTitle" :min-width="20">
+                <template v-slot="scope">
+                    <span v-if="!justShow" class="font-bold">{{ scope.$index }}.</span>
+                    <span v-else>{{ scope.row.rectangleType }}.{{ scope.$index }}</span>
+                </template>
+            </el-table-column>
 
-                <el-table-column label="" :min-width="20" v-if="editMode || !justShow">
-                    <template v-slot="scope">
-                        <span >
-                            <el-button type="default" @click="handleSaveRow(scope.$index)">確認</el-button>
-                        </span>
-                    </template>
-                </el-table-column>
+            <!-- Form Column -->
+            <el-table-column v-if="showFormColumn" :label="buttonText" :min-width="50">
+                <template v-slot="scope">
+                    <el-input :class="inputClass(scope.row)" :value="inputValue(scope.row)" @input="handleInput($event, scope.row)" @click="handleClick(scope.$index)">
+                        {{ inputValue(scope.row) }}
+                    </el-input>
+                </template>
+            </el-table-column>
 
-                <el-table-column label="" :min-width="20" v-if="editMode">
-                    <template v-slot="scope">
-                        <span >
-                            <a href="#" 
-                            @click.prevent="deleteShape(scope.row)" 
-                            title="Delete">
-                                <icon type="delete-shape" fill="red" />
-                            </a>
-                        </span>
-                    </template>
-                </el-table-column>
+            <!-- Save Button Column -->
+            <el-table-column v-if="showSaveButton" label="" :min-width="20">
+                <template v-slot="scope">
+                    <el-button type="default" @click="handleSaveRow(scope.$index)">確認</el-button>
+                </template>
+            </el-table-column>
 
-            </el-table>
-        </div>
+            <!-- Delete Shape Column -->
+            <el-table-column v-if="editMode" label="" :min-width="20">
+                <template v-slot="scope">
+                    <a href="#" @click.prevent="deleteShape(scope.row)" title="Delete">
+                        <icon type="delete-shape" fill="red" />
+                    </a>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 <style scoped>
 .disabled-input {
     opacity: 0.5;
+}
+.table-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    margin: 2px;
+    margin-bottom: 5px;
 }
 </style>
