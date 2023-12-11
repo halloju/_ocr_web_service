217805306b6b56458900bff4c9588346417d8117
halloju <wanchu.lin133@gmail.com>
2023-12-11 11:33:00 +0800

    [fix] cwe-918

--- backend/project_consume.py
+++ backend/project_consume.py
@@ -2,6 +2,7 @@ import redis
 from app.kafka_server.result_consumer import ResultConsumer
 import os
 import sys
+from urllib import parse
 from utils.logger import Logger
 
 logger_tool = Logger('consumer_main')
@@ -51,18 +52,32 @@ def run_consumer(project_name: str, redis_server, kafka_config):
             {'error_msg': f'consumer failed to start, error: {e}', 'project': project_name})
 
 
+def validate_redis_url(redis_url):
+    parse.uses_netloc.append('redis')
+    parsed_url = parse.urlparse(redis_url)
+    if parsed_url.scheme != 'redis':
+        raise ValueError("Invalid URL scheme for Redis URL")
+    if not parsed_url.hostname or not parsed_url.port:
+        raise ValueError("Invalid Redis URL")
+    return parsed_url
+
+
 if __name__ == "__main__":
-    from urllib import parse
     project_name = sys.argv[1]
     allowed_project_names = ['cv_controller', 'gp_controller']
     if project_name not in allowed_project_names:
         logger_tool.error("Not allowed consumer type")
         exit()
     redis_url = os.environ.get("LOCAL_REDIS_URL", "redis://localhost:6379")
-    parse.uses_netloc.append('redis')
-    url = parse.urlparse(redis_url)
+    try:
+        validated_url = validate_redis_url(redis_url)
+        # Proceed with using validated_url to establish Redis connection
+    except ValueError as e:
+        # Handle invalid URL appropriately
+        logger_tool.error(f"Error: {e}")
+        exit()
     redis_server = redis.Redis(
-        host=url.hostname, port=url.port, db=0, password=url.password, decode_responses=True)
+        host=validated_url.hostname, port=validated_url.port, db=0, password=validated_url.password, decode_responses=True)
     kafka_config = {
         'sasl.username': os.environ.get('KAFKA_ID'),
         'sasl.password': os.environ.get('KAFKA_PASSWORD'),
