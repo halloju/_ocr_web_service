5732aec14fd1c64e9425bd9928027ae0a5eaeae0
halloju <wanchu.lin133@gmail.com>
2023-12-28 14:48:26 +0800

    [fix] feedback status

--- backend/app/routers/feedback.py
+++ backend/app/routers/feedback.py
@@ -19,26 +19,34 @@ logger = Logger('feedback')
 
 
 # Modify your endpoint to accept a list of FeedbackRequest
-@router.post("/feedback", summary="批次回饋")
+from fastapi import FastAPI, Depends
+from typing import List
+import asyncio
+import uuid
+from your_module import FeedbackRequest, User, get_current_user, async_call_mlaas_function, logger
+
+app = FastAPI()
+
+@app.post("/feedback", summary="Batch Feedback")
 async def batch_feedback(
     feedback_requests: List[FeedbackRequest],
     current_user: User = Depends(get_current_user),
 ):
-    '''
-    Call feedback api for a batch of feedback requests
-    '''
     action = 'feedback/feedback'
     rid = str(uuid.uuid4())
     logger.logger.extra['request_id'] = rid
     logger.logger.extra['user_id'] = current_user.user_id
+
+    success_count, failure_count = 0, 0
+    failure_details = []
 
     try:
         batch_size = 10  # Set your batch size
 
         for i in range(0, len(feedback_requests), batch_size):
             batch = feedback_requests[i:i + batch_size]
-            
             batch_tasks = []
+
             for feedback in batch:
                 # Convert feedback to dict and update the user_id
                 feedback_data = feedback.dict(exclude_none=True)
@@ -54,17 +62,26 @@ async def batch_feedback(
                 # Add the task to the batch
                 task = async_call_mlaas_function(payload, action, project='service', logger=logger)
                 batch_tasks.append(task)
-                
+
             results = await asyncio.gather(*batch_tasks, return_exceptions=True)
-            # Process results and handle any exceptions
+
             for result in results:
                 if isinstance(result, Exception):
+                    failure_count += 1
+                    failure_details.append(str(result))
                     logger.error({'error_msg': str(result), 'action': action})
                 else:
+                    success_count += 1
                     logger.info(str(result))
 
-        return FeedbackResponse(status_code='0000', status_msg='Feedback Success')
+        if failure_count == len(feedback_requests):
+            return {"status_code": '4000', "status_msg": 'All feedback requests failed', "err_details": failure_details}
+        elif failure_count == 0:
+            return {"status_code": '0000', "status_msg": f'Feedback success.'}
+        else:
+            return {"status_code": '4001', "status_msg": f'Feedback partially successful. Success: {success_count}, Failures: {failure_count}', "err_details": failure_details}
 
     except Exception as ex:
         logger.error({'error_msg': str(ex), 'action': action})
-        return FeedbackResponse(status_code='4000', status_msg='feedback failed', err_detail=str(ex))
+        return {"status_code": '5000', "status_msg": 'Internal Server Error', "err_detail": str(ex)}
+
